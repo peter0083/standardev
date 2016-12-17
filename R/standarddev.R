2deb1a0f9f6cccfc99e3a63b224205af0079cdd3
ptylin <peter.tingyao@alumni.ubc.ca>
2016-12-17 15:59:40 -0800

    added more unit tests

--- R/standarddev.R
+++ R/standarddev.R
@@ -14,6 +14,8 @@
 #' vector_NHL_player_salaries <- c(4060000, 6590000, 5640000)
 #' # use this vector as an input for standard_deviation function#' standard_deviation(vector_NHL_player_salaries)
 standard_deviation <- function(x) {
+  if (length(x) == 0) stop("ZERO ERROR: standard_deviation cannot an empty list/vector as an input", call. = TRUE)
+  if (typeof(x) == "character") stop("TYPE ERROR: standard_deviation: input x has to be a numeric vector or list")
   n <- length(x)
   mean = sum(x) / n
   ssq <- sum((x-mean)^2)
--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(standardev)
 
 context("calculating standard deviation")
 
-test_that("standard_deviation calculates standard deviation from a vector", {
+test_that("standard_deviation calculates correct output", {
   x <- c(13, 23, 12, 44, 55)
   n <- length(x)
   mean = sum(x) / n
@@ -13,6 +13,34 @@ test_that("standard_deviation calculates standard deviation from a vector", {
 
   test_output <- standard_deviation(x)
 
+  expect_identical(standard_deviation(x), stddev1)
+  expect_identical(test_output, stddev1)
+})
+
+
+test_that("standard_deviation cannot an empty list/vector as an input", {
+  x <- c()
+  standard_deviation(x)
+  
+  expect_error(standard_deviation(x))
+})
+
+test_that("standard_deviation: input x has to be a numeric vector or list", {
+  x <- c("a", 6, "q")
+  standard_deviation(x)
+  
+  expect_error(standard_deviation(x))
+})
+
+test_that("standard_deviation: input can be a column from a data frame", {
+  x <- mtcars$cyl
+  n <- length(x)
+  mean = sum(x) / n
+  ssq <- sum((x-mean)^2)
+  stddev1 = sqrt(ssq/n)
+  return(stddev1)
+  testoutput <- standard_deviation(x)
+  
   expect_identical(standard_deviation(x), stddev1)
   expect_identical(test_output, stddev1)
 })
